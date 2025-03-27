eb8919b5b633bf9973db6343cdb5aa9e3469b1d6
Moses C. Nah <mosesnah@mit.edu>
2025-03-27 11:41:51 -0400

    update

--- MATLABApplications/main_iiwa14_singularity1.m
+++ MATLABApplications/main_iiwa14_singularity1.m
@@ -3,6 +3,7 @@ clear; close all; clc;
 
 % Make sure that you add Explicit-MATLAB before running this script!
 % Under "Explicit-MATLAB" directory, run setup.m file
+cd(fileparts(matlab.desktop.editor.getActiveFilename));
 
 % Configure default figure properties
 fig_config( 'fontSize', 20, 'markerSize', 10 )
